3106aff24c209f184e0856d0a7f2133297fddd53
Milena <angiemmarinb@gmail.com>
2025-03-04 20:20:50 -0500

    implementar funcion para agragar amigos

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+//1.Inicia declarando una variable de tipo array, que almacenará los nombres de los amigos ingresados.
+
 let amigos = []
 
+
+//Implementa una función para agregar amigos
+
+// 1.Capturar el valor del campo de entrada: 
+
+function agrgarAmigo() {
+    let amigoIngresoDeUsuario = parseInt(document.getElementById('amigo').value)
+}
