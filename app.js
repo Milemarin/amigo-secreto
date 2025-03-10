0e27138858a2bb640d1da66d23f7eb0a1ca96150
Milena <angiemmarinb@gmail.com>
2025-03-09 20:59:24 -0500

    Agregar lista de amigos

--- app.js
+++ app.js
@@ -1,14 +1,57 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-//1.Inicia declarando una variable de tipo array, que almacenará los nombres de los amigos ingresados.
+//Crear un array para almacenar los nombres
+//let amigoSecreto = "manuel";
 
-let amigos = []
-
+let listaAmigos = [];
 
 //Implementa una función para agregar amigos
 
-// 1.Capturar el valor del campo de entrada: 
-
-function agrgarAmigo() {
-    let amigoIngresoDeUsuario = parseInt(document.getElementById('amigo').value)
+//1. Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
+//2.Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
+
+    function agregarAmigo() {
+        let amigoUsuario = document.getElementById('amigo').value;
+        
+        if (amigoUsuario === '') {
+            alert('Por favor, ingrese un nombre válido');
+            return;
+        } else {
+            if(listaAmigos.includes(amigoUsuario)){
+            limpiar();
+            return; 
+        } else{
+            agragarAmigo();
+            limpiar();
+            return;
+        }
+        }
+    }   
+        
+//3.Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
+
+const li = (amigoUsuario) => `<li>${amigoUsuario}</li>`
+
+function agragarAmigo() {
+    listaAmigos.push(document.getElementById('amigo').value)
+    document.getElementById('listaAmigos').innerHTML = listaAmigos.map(amigoUsuario => li(amigoUsuario))
+}
+
+//4.Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
+
+function limpiar() {
+    document.getElementById('amigo').value = '';
+}
+    
+
+
+
+
+
+
+
